839c4116083324e954be0b905b383759f393db70
hrisse <72626109+hrisse@users.noreply.github.com>
2022-05-18 10:34:20 -0400

    add basic TechChat Listening

--- dart-cart-client/src/components/TechChat.tsx
+++ dart-cart-client/src/components/TechChat.tsx
@@ -1,18 +1,27 @@
 import { hasExpectedRequestMetadata } from "@reduxjs/toolkit/dist/matchers"
 import { render } from "@testing-library/react";
 import React, {useEffect, useState} from  'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-function TechChat () {
+function TechChat (props) {
+    const port = process.env.TECHCHAT_PORT;
     const [isConnected, setIsConnected] = useState(false);
-    let ws = new WebSocket('ws://localhost:3000/techchat');
-    useEffect(()=>{
-        ws.onmessage = (message) => {
-            console.log(message);
-        }
-    })
+    const [messages, setMessages] = useState([]);
+    const ws = new W3CWebSocket(`ws://localhost:${port}/techchat`);
+
+/*     useEffect(()=>{
+        ws.send(JSON.stringify({}))
+
+    },[props]) */
+
+    ws.onmessage = (message) => {
+        console.log(message);
+        setMessages(messages + message);
+    }
 
     return(
         <>
+        <p>Message:{messages}</p>
         </>
     )
 }
