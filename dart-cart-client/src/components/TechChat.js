fa8eb682e4b6a6f8180b85054f96748745cff725
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-05-18 23:22:08 -0400

    stomp is present and lightly functional

--- dart-cart-client/src/components/TechChat.js
+++ dart-cart-client/src/components/TechChat.js
@@ -1,4 +1,4 @@
-import { hasExpectedRequestMetadata } from "@reduxjs/toolkit/dist/matchers"
+// import { hasExpectedRequestMetadata } from "@reduxjs/toolkit/dist/matchers"
 import { render } from "@testing-library/react";
 import React, {useEffect, useState} from  'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
@@ -7,7 +7,17 @@ function TechChat (props) {
     const port = process.env.TECHCHAT_PORT;
     const [isConnected, setIsConnected] = useState(false);
     const [messages, setMessages] = useState([]);
-    const ws = new W3CWebSocket(`ws://localhost:${port}/techchat`);
+    const ws = new W3CWebSocket(`ws://localhost:3000/secured/room`);
+
+    useEffect(() => {
+        setMessages("Hello");
+
+    }, []);
+
+
+
+
+
 
 /*     useEffect(()=>{
         ws.send(JSON.stringify({}))
@@ -21,7 +31,8 @@ function TechChat (props) {
 
     return(
         <>
-        <p>Message:{messages}</p>
+            <p>Message:{messages}</p>
+            <button> </button>
         </>
     )
 }
