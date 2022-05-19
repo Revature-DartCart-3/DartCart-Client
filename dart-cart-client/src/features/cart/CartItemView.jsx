b934063fe94fccb0d47ac0d732aece4a4d42b494
Matthew Nevish <matthewnevish@gmail.com>
2022-05-19 11:50:06 -0500

    Added Delete Button

--- dart-cart-client/src/features/cart/CartItemView.jsx
+++ dart-cart-client/src/features/cart/CartItemView.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { CartItem, RootState } from "../../common/types"
 import { selectCartItemById, updateCartItem, updateCart } from "../../common/slices/cartSlice";
+import './cart.css';
 
 
 const CartItemView = ({ id }) => {
@@ -45,6 +46,24 @@ const CartItemView = ({ id }) => {
         }
     }
 
+    const handleDelete = (quantity) => {
+            dispatch(updateCartItem({
+                id: id,
+                changes: {
+                    quantity: quantity = 0
+                }
+            }))
+
+            dispatch(updateCart({
+                id: id,
+                changes: {
+                    quantity: quantity = 0
+                }
+            }))
+
+            window.location.reload()
+    }
+
     const saveQuantity = () => {
         if(item) dispatch(updateCart(item))
     }
@@ -66,8 +85,9 @@ const CartItemView = ({ id }) => {
                         <p className="card-text">{item?.shopProduct.product.description}</p>
                         <p className="card-text">${item?.shopProduct.price}</p>
                         <input className="form-control" type="number" value={item?.quantity} onChange={e => handleItemUpdate(e.target.value)} min={1} readOnly/>
-                        <button value={item?.quantity} onClick={e => handleQuantityAdd(e.target.value)}>+</button>
-                        <button value={item?.quantity} onClick={e => handleQuantitySubtract(e.target.value)}>-</button>
+                        <button className="button1" value={item?.quantity} onClick={e => handleQuantityAdd(e.target.value)}> + </button>
+                        <button className="button1" value={item?.quantity} onClick={e => handleQuantitySubtract(e.target.value)}> - </button>
+                        <button className="button1" value={item?.quantity} onClick={e => handleDelete(e.target.value)}> Delete </button>
                         <button onClick={saveQuantity}>Update</button>
                     </div>
                 </div>
