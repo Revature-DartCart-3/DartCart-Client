b6d5ad89b32c875f2089f77e69a672b240a82752
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-05-19 13:58:32 -0400

    added the probing of

--- dart-cart-client/src/components/TechChat.js
+++ dart-cart-client/src/components/TechChat.js
@@ -10,29 +10,55 @@ function TechChat (props) {
     const ws = new W3CWebSocket(`ws://localhost:3000/secured/room`);
 
     useEffect(() => {
-        setMessages("Hello");
+
+        //SET THE STAGE FOR USER ENTERING (BUTTON AND SIMPLE FORM -- FOR TESTING PURPOSES)
+        //1) GET USER'S ID UPON ENTERING THE CHAT(PRESSING THE BUTTON)
+        //--- IT WILL CREATE SESSION ID
+        //--- IT
+
+
+        // setMessages("Hello");
 
     }, []);
 
-
-
-
-
-
-/*     useEffect(()=>{
-        ws.send(JSON.stringify({}))
-
-    },[props]) */
 
     ws.onmessage = (message) => {
         console.log(message);
         setMessages(messages + message);
     }
 
+
+     function createSession() {
+        //NEED TO FIND THE EQUIVALENT OF AXIOS CALL WITH WEBSCOOTES
+         //USE CONNECT(), DISCONECT()
+
+         setMessages(
+             "will add the onopen stuff here"
+
+
+
+         )
+
+         ws.onopen = () => {
+
+
+         }
+
+
+
+         }
+
+
+
     return(
         <>
             <p>Message:{messages}</p>
-            <button> </button>
+            <button
+                //MAKE A PARMETER FOR THE USERID OR STATE WITH WS
+                onClick={createSession}
+            >
+                Enter Chat
+            </button>
         </>
     )
 }
