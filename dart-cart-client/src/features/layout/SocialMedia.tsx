404ec2a8b2c996f4b463656cdfe508d4844c368c
meganorth14 <meganorth14@gmail.com>
2022-05-22 14:22:08 -0700

    social media security hotspot fix

--- dart-cart-client/src/features/layout/SocialMedia.tsx
+++ dart-cart-client/src/features/layout/SocialMedia.tsx
@@ -7,10 +7,10 @@ const SocialMedia = () => {
       <h6 className="text-uppercase fw-bold mb-4" id="footerFix">
         <i className="fa fa-opencart me-3" id="footerFix"></i>Dart Cart
       </h6>
-      <a href="http://www.facebook.com" target="no_blank" className="fa fa-facebook-square" id="footerFix"></a>
-      <a href="http://www.twitter.com" target="no_blank" className="fa fa-twitter-square" id="footerFix"></a>
-      <a href="http://www.linkedin.com" target="no_blank" className="fa fa-linkedin-square" id="footerFix"></a>
-      <a href="http://www.pinterest.com" target="no_blank" className="fa fa-pinterest-square" id="footerFix"></a>
+      <a href="https://www.facebook.com" target="no_blank" className="fa fa-facebook-square" id="footerFix"></a>
+      <a href="https://www.twitter.com" target="no_blank" className="fa fa-twitter-square" id="footerFix"></a>
+      <a href="https://www.linkedin.com" target="no_blank" className="fa fa-linkedin-square" id="footerFix"></a>
+      <a href="https://www.pinterest.com" target="no_blank" className="fa fa-pinterest-square" id="footerFix"></a>
       <a href="https://github.com/Revature-DartCart" target="no_blank" className="fa fa-github" id="footerFix"></a>
 
     </>
