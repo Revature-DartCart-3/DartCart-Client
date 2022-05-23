6bb7b39ae5ac0a936c49e285634c8196e4045160
gtwnsnd20 <gtwnsnd20@gmail.com>
2022-05-23 11:50:15 -0500

    fixed useEffect

--- dart-cart-client/src/features/product-details/ShopProductDisplay.tsx
+++ dart-cart-client/src/features/product-details/ShopProductDisplay.tsx
@@ -5,7 +5,7 @@ import { selectShopProductById } from "../../common/slices/shopProductSlice";
 import { selectProductReviews, fetchProductReviews } from "../../common/slices/productReviewSlice";
 import { CompetingSellers } from "../competing-sellers/CompetingSellers";
 import { ProductReview } from "../../common/types";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAppDispatch } from "../../common/hooks";
 import ProductReviewDetail from "../product-reviews/ProductReviewDetail";
 import ProductReviewCard from "../product-reviews/product-review-card/ProductReviewCard";
@@ -20,45 +20,43 @@ const ShopProductDisplay = () => {
 
   const dispatch = useAppDispatch();
 
-  useEffect((): void => {
+  useEffect(() => {
     dispatch(fetchProductReviews(""));
-  }, [ReduxProductReviews]);
+  }, []);
 
   const updateProductReviews = () => {
     dispatch(fetchProductReviews(""));
-    console.log('callback');
   }
 
-  return (
-    <>
-      <div className="productInfoContainer">
-        <div className="productIMGcontainer">
-          <img className="testIMG" src={ReduxShopProducts?.imageURL} alt="Card image cap"></img>
-        </div> 
-        <div className="productName"><h1>{ReduxShopProducts?.name}</h1></div>
-        <div className="productDesc"><p>{ReduxShopProducts?.description}</p></div>
+  return (<>
+    <div className="productInfoContainer">
+      <div className="productIMGcontainer">
+        <img className="testIMG" src={ReduxShopProducts?.imageURL} alt="Card image cap"></img>
+      </div> 
+      <div className="productName"><h1>{ReduxShopProducts?.name}</h1></div>
+      <div className="productDesc"><p>{ReduxShopProducts?.description}</p></div>
+    </div>
+    <div className="sellersContainer">
+      <div className="sellerColumn">
+        <CompetingSellers Seller={ReduxShopProducts?.id!} />
       </div>
-      <div className="sellersContainer">
-        <div className="sellerColumn">
-          <CompetingSellers Seller={ReduxShopProducts?.id!}></CompetingSellers>
-        </div>
-      </div>
-      <div>
-        <ProductReviewDetail product_id={shop_product_id} callback={updateProductReviews} />
-      </div>
-      <table className="table">
-        {ReduxProductReviews.map((ProductReview) => { if (ProductReview.product.id==shop_product_id)
-                          return <ProductReviewCard 
-                          profilePic = {ProductReview.user.imageURL}
-                          title = {ProductReview.title}
-                          fullName = {ProductReview.user.lastName}
-                          comment = {ProductReview.comment}
-                          rating = {ProductReview.rating}
-                            />
-                        })}
-      </table>
-    </>
-  );
+    </div>
+    <div>
+      <ProductReviewDetail product_id={shop_product_id} callback={updateProductReviews} />
+    </div>
+    <table className="table">
+      {ReduxProductReviews.map((productReview) => { if (productReview.product.id==shop_product_id)
+                        return <ProductReviewCard 
+                        profilePic = {productReview.user.imageURL}
+                        title = {productReview.title}
+                        fullName = {productReview.user.lastName}
+                        comment = {productReview.comment}
+                        rating = {productReview.rating}
+                          />
+                      })}
+    </table>
+  </>);
 
 }
+
 export default ShopProductDisplay;
