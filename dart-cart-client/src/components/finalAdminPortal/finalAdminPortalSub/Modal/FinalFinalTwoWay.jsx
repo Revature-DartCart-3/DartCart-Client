7371f2c8534d5be06cec9957b6343f36ad33562d
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-05-24 21:38:46 -0400

    phrase 1

--- dart-cart-client/src/components/finalAdminPortal/finalAdminPortalSub/Modal/FinalFinalTwoWay.jsx
+++ dart-cart-client/src/components/finalAdminPortal/finalAdminPortalSub/Modal/FinalFinalTwoWay.jsx
@@ -1,46 +1,46 @@
-import { useState } from "react";
-import "./_modal.scss";
-import Modal from "./FinalFinalModal";
-
-function FinalFinalTwoWay() {
-  const [modal, setModal] = useState(false);
-
-  // const Toggle = () => setModal(!modal);
-  const Toggle = () => setModal(true);
-
-
-  var animateButton = function(e) {
-
-    e.preventDefault();
-    //reset animation
-    e.target.classList.remove('animate');
-    
-    e.target.classList.add('animate');
-    setTimeout(function(){
-      e.target.classList.remove('animate');
-    },700);
-  };
-  
-  var bubblyButtons = document.getElementsByClassName("bubbly-button");
-  
-  for (var i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
-
-  return (
-    <div className="App">
-      {/*<button className="clickMe bubbly-button" onClick={() => Toggle()}>*/}
-        <button className="clickMe bubbly-button" onClick="">
-        Tech Support
-      </button>
-
-      <Modal show={modal} close={Toggle} title="Tech Support Chat">
-        This is modal content for tech support
-
-      </Modal>
-    </div>
-
-  );
-}
-
-export default FinalFinalTwoWay;
+// import { useState } from "react";
+// import "./_modal.scss";
+// import Modal from "./FinalFinalModal";
+//
+// function FinalFinalTwoWay() {
+//   const [modal, setModal] = useState(false);
+//
+//   // const Toggle = () => setModal(!modal);
+//   const Toggle = () => setModal(true);
+//
+//
+//   var animateButton = function(e) {
+//
+//     e.preventDefault();
+//     //reset animation
+//     e.target.classList.remove('animate');
+//
+//     e.target.classList.add('animate');
+//     setTimeout(function(){
+//       e.target.classList.remove('animate');
+//     },700);
+//   };
+//
+//   var bubblyButtons = document.getElementsByClassName("bubbly-button");
+//
+//   for (var i = 0; i < bubblyButtons.length; i++) {
+//     bubblyButtons[i].addEventListener('click', animateButton, false);
+//   }
+//
+//   return (
+//     <div className="App">
+//       {/*<button className="clickMe bubbly-button" onClick={() => Toggle()}>*/}
+//         <button className="clickMe bubbly-button" onClick="">
+//         Tech Support
+//       </button>
+//
+//       <Modal show={modal} close={Toggle} title="Tech Support Chat">
+//         This is modal content for tech support
+//
+//       </Modal>
+//     </div>
+//
+//   );
+// }
+//
+// export default FinalFinalTwoWay;
--- dart-cart-client/src/features/TechChatModal/TechModal.js
+++ dart-cart-client/src/features/TechChatModal/TechModal.js
@@ -1,66 +1,66 @@
-import ReactDom from "react-dom";
-import "./finalModal.scss";
-import Axios from 'axios';
-import { useState } from 'react';
-
-const TechModal = ({ show, close, title, children }) => {
-  const [alias, setAlias] = useState('');
-  const [comment, setComment] = useState('');
-
-
-
-
-
-
-
-  const addComment = (event) => {
-    event.preventDefault();
-    Axios.post('http://localhost:3001/commentssection', {
-      alias: alias,
-      comment: comment,
-    }).then(() => {
-      setAlias('');
-      setComment('');
-    });
-  };
-
-  return ReactDom.createPortal(
-    <>
-      <div
-        className={`modalContainer ${show ? "show" : ""} `}
-        onClick={() => close()}
-      >
-        <div className="modal" onClick={(e) => e.stopPropagation()}>
-          <header className="modal_header">
-          <button className="modal-close" onClick={() => close()}>
-             Close
-         
-            </button>
-            <h2 className="modal_header-title ">{title}</h2>
-            <button className="close" onClick={() => close()}>
-            Close
-            </button>
-          </header>
-          <main className="modal_content">{children}</main>
-          <footer className="modal_footer">
-          <input
-              className=""
-              type="text"
-              name="comment"
-              required
-              value={comment}
-              onChange={(event) => {
-                setComment(event.target.value);
-              }}
-            />
-            <button className="submit ">Submit</button>
-          </footer>
-        </div>
-      </div>
-      
-    </>,
-    document.getElementById("modal")
-  );
-};
-
-export default TechModal;
+// import ReactDom from "react-dom";
+// import "./finalModal.scss";
+// import Axios from 'axios';
+// import { useState } from 'react';
+//
+// const TechModal = ({ show, close, title, children }) => {
+//   const [alias, setAlias] = useState('');
+//   const [comment, setComment] = useState('');
+//
+//
+//
+//
+//
+//
+//
+//   const addComment = (event) => {
+//     event.preventDefault();
+//     Axios.post('http://localhost:3001/commentssection', {
+//       alias: alias,
+//       comment: comment,
+//     }).then(() => {
+//       setAlias('');
+//       setComment('');
+//     });
+//   };
+//
+//   return ReactDom.createPortal(
+//     <>
+//       <div
+//         className={`modalContainer ${show ? "show" : ""} `}
+//         onClick={() => close()}
+//       >
+//         <div className="modal" onClick={(e) => e.stopPropagation()}>
+//           <header className="modal_header">
+//           <button className="modal-close" onClick={() => close()}>
+//              Close
+//
+//             </button>
+//             <h2 className="modal_header-title ">{title}</h2>
+//             <button className="close" onClick={() => close()}>
+//             Close
+//             </button>
+//           </header>
+//           <main className="modal_content">{children}</main>
+//           <footer className="modal_footer">
+//           <input
+//               className=""
+//               type="text"
+//               name="comment"
+//               required
+//               value={comment}
+//               onChange={(event) => {
+//                 setComment(event.target.value);
+//               }}
+//             />
+//             <button className="submit ">Submit</button>
+//           </footer>
+//         </div>
+//       </div>
+//
+//     </>,
+//     document.getElementById("modal")
+//   );
+// };
+//
+// export default TechModal;
